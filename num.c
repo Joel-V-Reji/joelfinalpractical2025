4e3408eb8eac74cbd86aa9fa92831edcd05650c1
Joel V Reji <joelvreji793@gmail.com>
2025-05-17 11:34:42 +0530

    Update num.c

--- num.c
+++ num.c
@@ -7,10 +7,7 @@ int main() {
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
-    
-    sum = num1 + num3;
-    
+
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
     return 0;
